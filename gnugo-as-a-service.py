284d3d4554aeb17305dbd35d23b97fcc129ec15d
Didier Marin <marin@heuritech.com>
2016-03-28 23:25:31 +0200

    add verbose and executable args

--- gnugo-as-a-service.py
+++ gnugo-as-a-service.py
@@ -1,18 +1,12 @@
 from flask import Flask
 from flask.ext.cors import CORS
-from flaskrun import flaskrun
+import optparse
 import os
 import re
 import json
 
-verbose = True
-
-gnugocmd = 'gnugo --boardsize 19 --mode gtp'
-to_gnugo, from_gnugo = os.popen2(gnugocmd)
-gtpnr = 1
-
 def gtp(command):
-    global gtpnr
+    global gtpnr, to_gnugo, from_gnugo, verbose
     cmd = str(gtpnr)+' '+command
     if verbose:
         print 'cmd: '+cmd
@@ -70,4 +64,58 @@ def gtpCommand(command):
     return msg, 200 if isok else 400
 
 if __name__ == '__main__':
-    flaskrun(app)
+
+    default_pathtognugo = '/usr/games/gnugo'
+    default_host="127.0.0.1"
+    default_port="5000"
+
+    # Set up the command-line options
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname of the Flask app " + \
+                           "[default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the Flask app " + \
+                           "[default %s]" % default_port,
+                      default=default_port)
+    parser.add_option("-e", "--executable",
+                      help="Path to the GnuGo executable " + \
+                           "[default %s]" % default_pathtognugo,
+                      default=default_pathtognugo)
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose",
+                      help=optparse.SUPPRESS_HELP)
+    
+    # Two options useful for debugging purposes, but 
+    # a bit dangerous so not exposed in the help message.
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+    parser.add_option("-p", "--profile",
+                      action="store_true", dest="profile",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    # If the user selects the profiling option, then we need
+    # to do a little extra setup
+    if options.profile:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+
+        app.config['PROFILE'] = True
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
+                       restrictions=[30])
+        options.debug = True
+
+    
+    gnugocmd = options.executable + ' --boardsize 19 --mode gtp'
+    to_gnugo, from_gnugo = os.popen2(gnugocmd)
+    verbose = options.verbose 
+    gtpnr = 1
+
+    app.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
